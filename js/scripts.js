377f1e556039631c12754a3ae44e9b9e7fef82ee
lucianoadorni <142657836+lucianoadorni@users.noreply.github.com>
2024-10-25 19:45:34 +0200

    Added more splide stuff.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+new Splide( '.splide', {
+    type   : 'loop',
+    perPage: 3,
+    width: "100vw",
+    height: "50vh",
+    perMove: 1,
+    focus: "center",
+    gap: "1em",
+    autoWidth: true,
+}).mount();
