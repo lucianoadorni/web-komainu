fe2f6a702bac28da1e5b9bdeaf01eb814dfabe4c
lucianoadorni <142657836+lucianoadorni@users.noreply.github.com>
2024-11-01 21:18:03 +0100

    Added reserve page + now the carrousel works

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 new Splide( '.splide', {
     type   : 'loop',
     perPage: 3,
-    width: "100vw",
-    height: "50vh",
     perMove: 1,
     focus: "center",
     gap: "1em",
+    autoWidth: true,
 }).mount();
