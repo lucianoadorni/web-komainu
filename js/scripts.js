876de95bab511fcd1fc4df34a8df4c69a641b417
xiangsoto <xiangsoto.145@gmail.com>
2024-11-05 03:16:59 +0100

    formulario p2, validación intro, cards, correcciones, media queries

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,7 @@ new Splide( '.splide', {
 }).mount();
 
 
-
 // OVERLAY Y SPINNER
-
 const reservarMesaBtn = document.querySelector('.reservar-mesa');
 
 reservarMesaBtn.addEventListener('click', function(event) {
@@ -23,3 +21,26 @@ reservarMesaBtn.addEventListener('click', function(event) {
         window.location.href = 'reservar.html';
     }, 2000);
 });
+
+// VENTANA MODAL
+
+
+// LO QUE SALE EN LA GUÍA DE BOOTSTRAP pero hay q modificar ig
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
